1fede1e61f2ce51fe9336417be67e6b266508bc8
pratikartik <kartikshikhare26@gmail.com>
2022-10-12 01:04:11 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
   const yse = '11';
   return (
-    <div className="App">
+<div className="App">
       <div>
         <img src="assets/crumb.svg" />
         <h2>CRUMB</h2>
