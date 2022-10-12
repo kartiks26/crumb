87bae612936541443553ab34700aa5d595b94c55
pratikartik <kartikshikhare26@gmail.com>
2022-10-12 20:15:28 +0530

    husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    amd: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-    'no-console': 'error',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
 };
