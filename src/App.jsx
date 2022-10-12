0e083a732a238d1d999cc2eb10aafd97136a8b6c
pratikartik <kartikshikhare26@gmail.com>
2022-10-13 00:19:45 +0530

    added Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
-import './App.scss';
+import { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [counts, setCounts] = useState(0);
-	const yse = '11';
-	return (
-		<div className="App">
-			<div>
-				<img src="assets/crumb.svg" />
-				<h2>CRUMB</h2>
-				<p>Get started with your NFT journey. NOW!</p>
-			</div>
-		</div>
-	);
+  const [count, setCount] = useState(0);
+  const [counts, setCounts] = useState(0);
+  const yse = "11";
+  return (
+    <div className="App">
+      <div>
+        <img src="assets/crumb.svg" />
+        <h2>CRUMB</h2>
+        <p>Get started with your NFT journey. NOW!</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
